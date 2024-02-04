ce3bdf7ac57a6c5d941a5148e14411e92544a210
faritg1 <juanagelves@gmail.com>
2024-02-03 22:29:56 -0500

    feat: :hammer: add config script bg-img and bg-color

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,35 @@
 module.exports = {
   content: ["./public/**/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage : {
+        'bali': "url('./public/img/bali.jpg')",
+        'chicago': "url('./public/img/chicago.jpg')",
+        'europe': "url('./public/img/europe.jpg')",
+        'iceland': "url('./public/img/iceland.jpg')",
+        'LA': "url('./public/img/LA.jpg')",
+        'miami': "url('./public/img/miami.jpg')",
+        'new_york': "url('./public/img/new_york.jpg')",
+        'norway': "url('./public/img/norway.jpg')",
+        'sanFrancisco': "url('./public/img/sanFrancisco.jpg')",
+        'sanFranciscoDesktop': "url('./public/img/sanFranciscoDesktop.jpg')",
+        'seattle': "url('./public/img/seattle.jpg')",
+        'switzerland': "url('./public/img/switzerland.jpg')",
+        'sydney': "url('./public/img/sydney.jpg')",
+        'yosemite': "url('./public/img/yosemite.jpg')"
+      },
+      backgroundColor: theme =>  ({
+        ...theme('colors'),
+        'primary': '#CC2D4A', 
+        'secondary': '#8FA206', 
+        'tertiary': '#61AEC9', 
+      }), 
+        textColor:{ 
+          'primary': '#CC2D4A', 
+          'secondary': '#8FA206', 
+          'tertiary': '#61AEC9'
+      }
+    },
   },
   plugins: [],
 }
